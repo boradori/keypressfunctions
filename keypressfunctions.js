4e4a65070f5a9a5c5a2668ec59449883cd2bee3a
boradori <youngsunpaik@gmail.com>
2015-03-07 12:24:48 -0800

    keypressfunction

--- keypressfunctions.js
+++ keypressfunctions.js
@@ -1,1 +1,12 @@
-// function
+window.addEventListener("keydown", checkKeyPressed, false);
+
+function checkKeyPressed(e) {
+    if (e.keyCode == "13") {
+        // alert("The 'ENTER' key is pressed.");
+        console.log("The 'ENTER' key is pressed.");
+    } else if (e.keyCode =="32") {
+        console.log("The 'SPACE' key is pressed.");
+    } else if (e.keyCode =="9") {
+        console.log("The 'TAB' key is pressed.");
+    }
+}

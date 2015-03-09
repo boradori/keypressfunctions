a9885a556b740c0d1027339697a9010e356425b6
boradori <youngsunpaik@gmail.com>
2015-03-09 16:14:08 -0700

    Added angular directives

--- keypressfunctions.js
+++ keypressfunctions.js
@@ -1,12 +1,34 @@
-window.addEventListener("keydown", checkKeyPressed, false);
+(function() {
+    var app = angular.module('keypress', []); // Module keypress
 
-function checkKeyPressed(e) {
-    if (e.keyCode == "13") {
-        // alert("The 'ENTER' key is pressed.");
-        console.log("The 'ENTER' key is pressed.");
-    } else if (e.keyCode =="32") {
-        console.log("The 'SPACE' key is pressed.");
-    } else if (e.keyCode =="9") {
-        console.log("The 'TAB' key is pressed.");
-    }
-}
+    app.controller('mainController', function($scope) { // Controller
+
+        $scope.onKeyPress = function ($event) {
+            if ($event.keyCode == "13") {
+                console.log("The 'ENTER' key is pressed.");
+            } else if ($event.keyCode == "32") {
+                console.log("The 'SPACE' key is pressed.");
+            } else if ($event.keyCode == "9") {
+                console.log("The 'TAB' key is pressed.");
+            } else if ($event.keyCode == "8") {
+                console.log("The 'BACKSPACE' key is pressed.");
+            } else {
+                console.log("I only console.log ENTER, SPACE, TAB, and BACKSPACE!")
+            }
+        }
+    });
+})(); // For some reason, TAB and BACKSPACE do not work.
+
+
+// window.addEventListener("keydown", checkKeyPressed, false);
+
+// function checkKeyPressed(e) {
+//     if (e.keyCode == "13") {
+//         alert("The 'ENTER' key is pressed.");
+//         console.log("The 'ENTER' key is pressed.");
+//     } else if (e.keyCode =="32") {
+//         console.log("The 'SPACE' key is pressed.");
+//     } else if (e.keyCode =="9") {
+//         console.log("The 'TAB' key is pressed.");
+//     }
+// }
